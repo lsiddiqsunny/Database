576e70dc4f557e68b87b6725ceba220d1d64ddb4
Latif siddiq sunny <1505069.mls@ugrad.cse.buet.ac.bd>
2017-11-15 23:24:17 +0600

    Update _project_schema.sql

--- Database project/_project_schema.sql
+++ Database project/_project_schema.sql
@@ -2,21 +2,21 @@
 
 CREATE TABLE Location(
 Location_id INT PRIMARY KEY,
-Street_address VARCHAR2(100),
+Street_address VARCHAR2(255),
 Post_code INT,
-City VARCHAR2(20)
+City VARCHAR2(255)
 );
 
 --Department
 CREATE TABLE Department(
 Department_id INT PRIMARY KEY,
-Department_name VARCHAR2(30)
+Department_name VARCHAR2(255)
 );
 
 --Job
 CREATE TABLE Job(
 Job_id INT PRIMARY KEY,
-Job_name VARCHAR2(30),
+Job_name VARCHAR2(255),
 Salary INT,
 Department_id REFERENCES Department(Department_id)
 );
@@ -24,8 +24,7 @@ Department_id REFERENCES Department(Department_id)
 --Branch
 CREATE TABLE Branch(
 Branch_id INT PRIMARY KEY,
-Branch_name VARCHAR2(30),
-Location_id REFERENCES Location(Location_id)
+Branch_name VARCHAR2(255)
 );
 
 
@@ -33,12 +32,12 @@ Location_id REFERENCES Location(Location_id)
 --Employee
 CREATE TABLE Employee(
 Employee_id INT PRIMARY KEY,
-Employee_name VARCHAR2(30),
-Email VARCHAR2(100),
-Phone_number VARCHAR2(20),
+Employee_name VARCHAR2(255),
+Email VARCHAR2(255),
+Phone_number VARCHAR2(30),
+Password VARCHAR2(100),
 Location_id REFERENCES Location(Location_id),
-Job_id REFERENCES Job(Job_id),
-Department_id REFERENCES Department(Department_id)
+Job_id REFERENCES Job(Job_id)
 );
 
 --Job history
@@ -56,18 +55,39 @@ CONSTRAINT JOB_HSITORY_PK PRIMARY KEY (Employee_id,Job_id,Department_id,Branch_i
 --Transport
 CREATE TABLE Transport(
 Transport_id INT PRIMARY KEY,
-License VARCHAR2(50),
+License VARCHAR2(255),
 Weight_Capacity INT,
 Location_id REFERENCES Location(Location_id),
 Employee_id REFERENCES Employee(Employee_id),
 Branch_id REFERENCES Branch(Branch_id)
 );
 
+--Book storage 
+CREATE TABLE Book_storage(
+Storage_id INT PRIMARY KEY,
+Book_Capacity INT,
+Location_id REFERENCES Location(Location_id),
+Employee_id REFERENCES Employee(Employee_id),
+Branch_id REFERENCES Branch(Branch_id)
+);
+
+
 --Publisher
 CREATE TABLE Publisher(
 Publisher_id INT PRIMARY KEY,
-Publisher_name VARCHAR2(30),
+Publisher_name VARCHAR2(255),
 Location_id REFERENCES Location(Location_id)
+);
+
+--Book
+CREATE TABLE Book(
+Book_id INT PRIMARY KEY,
+Book_name VARCHAR2(500),
+Price Number,
+Commision Number(2,2),
+Total_in_storage INT,
+Publisher_id REFERENCES Publisher(Publisher_id),
+Storage_id REFERENCES Book_storage(Storage_id)
 );
 
 --Publisher Transaction
@@ -82,33 +102,15 @@ Publisher_id REFERENCES Publisher(Publisher_id)
 --Customer
 CREATE TABLE Customer(
 Customer_id INT PRIMARY KEY,
-Customer_name VARCHAR2(30),
-Email VARCHAR2(100),
-Phone_number VARCHAR2(20),
+Customer_name VARCHAR2(255),
+Email VARCHAR2(255),
+Phone_number VARCHAR2(30),
 Password VARCHAR2(100),
 Location_id REFERENCES Location(Location_id),
 Branch_id REFERENCES Branch(Branch_id)
 );
 
---Book storage 
-CREATE TABLE Book_storage(
-Storage_id INT PRIMARY KEY,
-Book_Capacity INT,
-Location_id REFERENCES Location(Location_id),
-Employee_id REFERENCES Employee(Employee_id),
-Branch_id REFERENCES Branch(Branch_id)
-);
 
---Book
-CREATE TABLE Book(
-Book_id INT PRIMARY KEY,
-Book_name VARCHAR2(500),
-Price INT,
-Commision Number(2,2),
-Total_in_storage INT,
-Publisher_id REFERENCES Publisher(Publisher_id),
-Storage_id REFERENCES Book_storage(Storage_id)
-);
 
 --Customer transaction
 CREATE TABLE Customer_order(
@@ -127,4 +129,21 @@ Time DATE,
 Review VARCHAR2(1000),
 Customer_id REFERENCES Customer(Customer_id),
 Book_id REFERENCES Book(Book_id)
+);
+
+
+
+--Office material
+CREATE TABLE Office_material(
+Material_id INT PRIMARY KEY,
+Type VARCHAR2(100)
+);
+
+--Office material amount
+CREATE TABLE material_amount(
+Branch_id  REFERENCES Branch(Branch_id),
+Material_id  REFERENCES Office_material(Material_id),
+Amount INT,
+CONSTRAINT material_amount_PK PRIMARY KEY (Branch_id,Material_id)
+
 );
