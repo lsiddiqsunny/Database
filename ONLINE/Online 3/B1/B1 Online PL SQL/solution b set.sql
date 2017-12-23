fb1aa12aabf4ffa5588ee007d3d0e75f5b57d3cb
Latif siddiq sunny <lsiddiqsunny@gmail.com>
2017-12-24 01:31:21 +0600

    new solution for online 3 section B1

---
+++ ONLINE/Online 3/B1/B1 Online PL SQL/solution b set.sql
@@ -0,0 +1,69 @@
+1.CREATE OR REPLACE FUNCTION default_working_project (MID IN VARCHAR2)
+RETURN VARCHAR2 IS
+
+MSG VARCHAR2(100) ;
+AVGSAL NUMBER;
+BEGIN
+select avg(salary) INTO AVGSAL
+FROM EMP
+GROUP BY DEPARTMENT_ID
+HAVING DEPARTMENT_ID=MID;
+IF  AVGSAL>10500 THEN
+MSG:='GRAND';
+ELSIF AVGSAL>9000 AND AVGSAL<10500 THEN
+MSG:='MEDIUM';
+ELSE
+MSG:='SMALL';
+END IF;
+
+RETURN MSG ;
+EXCEPTION
+--you must return value from this section also
+WHEN NO_DATA_FOUND THEN
+RETURN 'NULL DEPT' ;
+END ;
+/
+2.CREATE OR REPLACE FUNCTION default_salary  (MID IN VARCHAR2,Wproject IN VARCHAR2)
+RETURN VARCHAR2 IS
+
+MSG VARCHAR2(100) ;
+SAL NUMBER;
+BEGIN
+IF Wproject IS NULL THEN
+MSG:=default_working_project(MID);
+END IF;
+IF MSG ='GRAND' THEN
+SAL:=14000;
+ELSIF MSG = 'MEDIUM' THEN
+SAL:=10000;
+ELSE
+SAL:=6000;
+END IF;
+RETURN SAL ;
+END ;
+/
+3.
+CREATE OR REPLACE TRIGGER check_entry
+BEFORE INSERT
+ON EMP
+FOR EACH ROW
+DECLARE
+CO number;
+BEGIN
+select count(*) into CO
+from EMP
+WHERE DEPARTMENT_ID= :new.DEPARTMENT_ID
+GROUP BY DEPARTMENT_ID;
+IF  :new.salary is Null THEN
+ :new.SALARY := default_salary (:new.manager_id,:new.working_project);
+ELSIF :new.salary is not Null THEN
+:new.working_project :=default_working_project (:new.manager_id);
+ELSE
+DBMS_OUTPUT.PUT_LINE('no change');
+END IF;
+EXCEPTION
+--you must return value from this section also
+WHEN NO_DATA_FOUND THEN
+ DBMS_OUTPUT.PUT_LINE('DO NOTHING');
+END ;
+/
